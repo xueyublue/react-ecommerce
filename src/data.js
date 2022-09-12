6d47bf553a040f6b55803a0dc86f17c738839fe3
Xue Yu <xueyublue@gmail.com>
2022-09-12 17:42:51 +0800

    Added Category

--- src/data.js
+++ src/data.js
@@ -21,3 +21,27 @@ export const sliderItems = [
     bg: "f5fafd",
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+    title: "SUMMER SALE",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+    title: "AUTUMN COLLECTION",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+    title: "OTHER SALES",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+];
