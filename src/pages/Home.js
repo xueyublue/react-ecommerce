db498251b250f8e322ffa382bb6c98992be48a22
Xue Yu <xueyublue@gmail.com>
2022-09-13 10:40:58 +0800

    Completed Product

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import NavBar from "./../components/NavBar";
 import Announcement from "./../components/Announcement";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
+import Products from "./../components/Products";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <NavBar />
       <Slider />
       <Categories />
+      <Products />
     </div>
   );
 };
